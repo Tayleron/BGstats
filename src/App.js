c7e861b1fc40e1ff0b1a47ef84a44165dd74c6ce
Tayleron <tayleron@gmail.com>
2017-12-07 15:37:23 -0330

    Static HTML-style webpage complete

--- src/App.js
+++ src/App.js
@@ -24,9 +24,29 @@ class App extends React.Component {
 
   render() {
     console.log(this.state)
-    
     return(
-      null
+      <div id="container">
+        <header>
+          <h1>Board game Night Stats</h1>
+        </header>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Users</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Stats</a></li>
+          </ul>
+        </nav>
+        <div id="main">
+          <form action="" id="playerCreate">
+            Create a new Player:
+            <input type="text" name="playerName" placeholder="Name"/>
+            <input type="submit" value="Create" onClick="player()"/>
+          </form>
+        </div>
+        <footer>{"Copyright Andrew Robinson " + String.fromCharCode(169) + " 2017"}
+        </footer>
+      </div>
     )
   }
 
