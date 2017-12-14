9ab39777f006ef19a82ad950678aeda2e4086e6a
Tayleron <tayleron@gmail.com>
2017-12-14 14:46:13 -0330

    Reduced clutter for work purposes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import Taskbar from "./taskbar.js";
 import DisplayPlayers from "./displayPlayers.js";
 import DisplayGames from "./displayGames.js";
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      players: 
-      [
+      players: [
         {
           "name": 
           {
@@ -25,30 +25,30 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state)
-    return(
+    return(       
+      console.log(this.state),
       <div id="container">
-        <header>
+        {/* <header>
           <h1>The Haunted Household</h1>
           <br/>
           <h1>Board Game Night Stats</h1>
-        </header>
-        <nav>
+        </header> */}
+        {/* <nav>
           <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">Users</a></li>
             <li><a href="#">Games</a></li>
             <li><a href="#">Stats</a></li>
           </ul>
-        </nav>
+        </nav> */}
         <div id="main">
           <Taskbar />
           <DisplayPlayers />
           <DisplayGames />
         </div>
-        <footer>
+        {/* <footer>
           {"Copyright Andrew Robinson " + String.fromCharCode(169) + " 2017"}
-        </footer>
+        </footer> */}
       </div>
     )
   }
